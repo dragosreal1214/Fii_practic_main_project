72a02480b8e34098077fe60b49223abac01a99f8
dragosreal1214 <dragos121416@gmail.com>
2022-03-12 13:44:25 +0200

    js

--- js_main.js
+++ js_main.js
@@ -1,3 +1,29 @@
-const title =document.getElementsByName('h1')[0];
 
-console.log(title.textContent=title.textContent + '!')
+const calendar=document.getElementById('calendar-wrap')
+//calendar.removeChild(calendar, lastElementChild);
+
+const section = document.createAttribute.Element('section');
+
+calendar.appendChild(section);
+
+const labels = ['Su','Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa',
+];
+
+for(let i = 0; i<labels.length;i++) {
+    const div= document.createElement('div');
+    div.textContent=labels[i];
+}
+
+const date= new Date();
+date.setDate(1);
+const currentMonth= date.getMonth();
+
+let day=1;
+while(date.getMonth() === currentMonth){
+    const div = document.createElement('div');
+    div.textContent =day;
+    section.appendChild(div);
+    date.setDate(++day);
+}
+
+calendar.appendChild(section);
